c1a951e76bc8007ef9df5cdbea991579f4376c24
University <university@AA687692-Mac.local>
2023-08-01 15:37:50 -0500

    connected to sql database and added get all depts query

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 const inquirer = require("inquirer")
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  // Your username
+  user: "root",
+  // Your password
+  password: "Bean123!",
+  database: "employee_db"
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+});
 
 function MainMenu(){
     inquirer
@@ -11,10 +25,42 @@ function MainMenu(){
     },
     
   ])
-  .then((response) =>
-    response.confirm === response.password
-      ? console.log('Success!')
-      : console.log('You forgot your password already?!')
-  );
+  .then((response) =>{
+    switch (response.selection) {
+        case "view all departments":     
+        console.log("Departments selected")
+        getdepartments();
+        MainMenu();
+        break;
+        case "view all roles":     
+        console.log("view all roles")
+        break;
+        case "view all employees":     
+        console.log("view all employees")
+        break;
+        case "add employee":     
+        console.log("add employee")
+        break;
+        case "add role":     
+        console.log("add role")
+        break;
+        case "add department":     
+        console.log("add department")
+        break;
+        case "update employee":     
+        console.log("update employee")
+        break;
+        default:
+        console.log("Please make a selection")
+    }
+});
+
+}
+async function getdepartments(){
+
+    let query = "SELECT * from departments"
+    const departments = await connection.query(query)
+
+    console.table(departments)
 
 }
