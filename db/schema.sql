2ba8ed5269316d87c93433f27986356bfea1f75e
University <university@AA687692-Mac.local>
2023-08-01 14:39:14 -0500

    finished schema table

--- db/schema.sql
+++ db/schema.sql
@@ -10,7 +10,15 @@ CREATE TABLE role (
     id	INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(30),
     salary DECIMAL,
-    
+     FOREIGN KEY (department_id) REFERENCES department(id)
 );
-CREATE TABLE department (
-    id	INT PRIMARY KEY AUTO_INCREMENT,
+
+CREATE TABLE employee (
+    id INT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
